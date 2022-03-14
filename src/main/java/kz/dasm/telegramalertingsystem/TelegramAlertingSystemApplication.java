c53e1071df4261d2226dbf75b47c9374ffa58f43
DAsm <zh.daur@gmail.com>
2022-03-14 14:21:41 +0600

    init

---
+++ src/main/java/kz/dasm/telegramalertingsystem/TelegramAlertingSystemApplication.java
@@ -0,0 +1,13 @@
+package kz.dasm.telegramalertingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramAlertingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramAlertingSystemApplication.class, args);
+	}
+
+}
