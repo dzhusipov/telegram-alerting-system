bba08db0bd74587b43e6dd1bd05df73364816c3d
DAsm <zh.daur@gmail.com>
2022-03-24 11:40:54 +0600

    try to make soap service

---
+++ src/main/java/kz/dasm/telegramalertingsystem/service/TelegramSoapService.java
@@ -0,0 +1,20 @@
+package kz.dasm.telegramalertingsystem.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class TelegramSoapService {
+    private static final String NAMESPACE_URI = "http://telegramalertingsystem.dasm.kz/";
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "sendNessageRequest")
+    @ResponsePayload
+    public SendMessageResponse sendNessage(@RequestPayload SendMessageRequest sendMessageRequest) {
+        SendMessageResponse response = new SendMessageResponse();
+        response.setReturn(request.getMessage());
+        return response;
+    }
+}
