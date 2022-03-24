35d2765e3c9ac559cc6bf3932cb87a4395ea1e0f
Жусипов Даурхан Асханович <Daurkhan.Zhussipov@kaspi.kz>
2022-03-24 14:55:58 +0600

    soap working but error is in Implementation of JAXB-API has not been found on module path or classpath

--- src/main/java/kz/dasm/telegramalertingsystem/service/TelegramSoapService.java
+++ src/main/java/kz/dasm/telegramalertingsystem/service/TelegramSoapService.java
@@ -1,6 +1,6 @@
 package kz.dasm.telegramalertingsystem.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import kz.dasm.telegramalertingsystem.ObjectFactory;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -9,15 +9,18 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import kz.dasm.telegramalertingsystem.SendMessageRequest;
 import kz.dasm.telegramalertingsystem.SendMessageResponse;
 
+import javax.xml.bind.JAXBElement;
+
 @Endpoint
 public class TelegramSoapService {
     private static final String NAMESPACE_URI = "http://telegramalertingsystem.dasm.kz/";
 
-	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "sendNessageRequest")
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "SendMessageRequest")
     @ResponsePayload
-    public SendMessageResponse sendNessage(@RequestPayload SendMessageRequest sendMessageRequest) {
+    public JAXBElement<SendMessageResponse> sendMessage(@RequestPayload SendMessageRequest sendMessageRequest) {
         SendMessageResponse response = new SendMessageResponse();
         response.setReturn(sendMessageRequest.getMessage());
-        return response;
+        ObjectFactory factory = new ObjectFactory();
+        return factory.createSendMessageResponse(response);
     }
 }
